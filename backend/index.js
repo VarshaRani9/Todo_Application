dc31f377f1e05fc4108ae7ceb23962e2eb450f8b
Varsha Rani <varsharanijnv16122001@gmail.com>
2024-07-10 14:19:34 +0530

    Added frontend origin in server

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,8 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    // origin: "http://localhost:5173",
+    origin: "https://todo-application-frontend-yctq.onrender.com",
   })
 );
 app.use(express.json());
